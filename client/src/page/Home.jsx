d209abf3ae88a5685c5351f3c84b84abd938d5b1
chaipawn <chaiyapawn.n@gmail.com>
2023-01-07 12:55:03 +0700

    setup layout using HOC and add create-battle page

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 
+import { PageHOC } from "../components";
+
 const Home = () => {
   return (
     <div>
-      <h1 className="text-5xl p-3">Avax Gods</h1>
-      <h2 className="text-3xl p-3">Web3 NFT Battle-style Card Game</h2>
+      <h1 className="text-white text-xl">Hello From Home</h1>
     </div>
   );
 };
 
-export default Home;
+export default PageHOC(
+  Home,
+  <>
+    Welcome to Avax Gods <br /> a Web3 NFT Card Game
+  </>,
+  <>
+    Connect your wallet to start playing <br /> the ultimate Web3 Battle Card
+    Game
+  </>
+);
