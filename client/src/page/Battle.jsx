cb5df06ab9c0af7002e21b5cde2bd7e3d92507ac
chaipawn <chaiyapawn.n@gmail.com>
2023-01-08 13:53:08 +0700

    make attack or defense functionality

--- client/src/page/Battle.jsx
+++ client/src/page/Battle.jsx
@@ -79,6 +79,21 @@ function Battle() {
     if (contract && gameData.activeBattle) getPlayerInfo();
   }, [contract, gameData, battleName]);
 
+  const makeAMove = async (choice) => {
+    playAudio(choice === 1 ? attackSound : defenseSound);
+
+    try {
+      await contract.attackOrDefendChoice(choice, battleName);
+      setShowAlert({
+        status: true,
+        type: "info",
+        message: `Initiating ${choice === 1 ? "attack" : "defense"}`,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div
       className={`${styles.flexBetween} ${styles.gameContainer} ${battleGround}`}
@@ -95,7 +110,7 @@ function Battle() {
         <div className="flex items-center flex-row">
           <ActionButton
             imgUrl={attack}
-            handleClick={() => {}}
+            handleClick={() => makeAMove(1)}
             restStyles="mr-2 hover:border-yellow-400"
           />
           <Card
@@ -106,7 +121,7 @@ function Battle() {
           />
           <ActionButton
             imgUrl={defense}
-            handleClick={() => {}}
+            handleClick={() => makeAMove(2)}
             restStyles="ml-6 hover:border-red-600"
           />
         </div>
